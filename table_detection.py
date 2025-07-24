9b9879ff6971d61316525e751ef267e9317bcfba
Abdumannon0406 <halimovabdumannon0406@gmail.com>
2025-07-24 19:35:02 +0500

    Table cell detection module

---
+++ table_detection.py
@@ -0,0 +1,7 @@
+from paddleocr import TableCellsDetection
+model = TableCellsDetection(model_name="RT-DETR-L_wired_table_cell_det")
+output = model.predict("table.png", threshold=0.3, batch_size=1)
+for res in output:
+    res.print(json_format=False)
+    res.save_to_img("output/")
+    res.save_to_json("output/table_res.json")
